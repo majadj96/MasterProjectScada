f17a5b66c84537f743e4d9760afb29cc55a73121
Kristina Polih <christina.polih@yahoo.com>
2019-05-07 18:51:29 +0200

    Simulation made and tested for fluid tanks

--- SCADA/FrontEndProcessorService/Simulator/SimulatorCommandingService.cs
+++ SCADA/FrontEndProcessorService/Simulator/SimulatorCommandingService.cs
@@ -6,6 +6,7 @@ using ScadaCommon.NDSDataModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace FrontEndProcessorService.Simulator
 {
@@ -13,6 +14,10 @@ namespace FrontEndProcessorService.Simulator
     {
         private Dictionary<Tuple<ushort, PointType>, BasePointCacheItem> model;
         private IFunctionExecutor functionExecutor;
+        private List<DigitalPointCacheItem> substationOne = new List<DigitalPointCacheItem>();
+        private List<DigitalPointCacheItem> substationTwo = new List<DigitalPointCacheItem>();
+        AnalogPointCacheItem tank1 = null;
+        AnalogPointCacheItem tank2 = null;
 
         public SimulatorCommandingService(IFunctionExecutor functionExecutor)
         {
@@ -27,10 +32,12 @@ namespace FrontEndProcessorService.Simulator
             else
                 model.Add(tuple, pointCacheItem);
 
+            FillLists();
+
             if (pointCacheItem.MeasurementType == Common.MeasurementType.SwitchStatus)
             {
                 if (String.Compare(pointCacheItem.MrId, "Breaker_1SwitchStatus") == 0 || String.Compare(pointCacheItem.MrId, "Breaker_Pump1") == 0 ||
-                   String.Compare(pointCacheItem.MrId, "Discrete_Disc1") == 0 || String.Compare(pointCacheItem.MrId, "Discrete_Disc2") == 0 )
+                   String.Compare(pointCacheItem.MrId, "Discrete_Disc1") == 0 || String.Compare(pointCacheItem.MrId, "Discrete_Disc2") == 0)
                 {
                     SetCurrentAndVoltage();
                 }
@@ -39,39 +46,104 @@ namespace FrontEndProcessorService.Simulator
                     SetCurrentAndVoltageSubTwo();
                 }
             }
-        }
 
-        public void SetCurrentAndVoltage()
-        {
-            List<DigitalPointCacheItem> lista = new List<DigitalPointCacheItem>();
 
-            foreach(var c in model.Values.Where(x => x.Type == PointType.BINARY_OUTPUT).ToList())
+            if (tank1 == null)
             {
-                if (String.Compare(c.MrId, "Breaker_1SwitchStatus") == 0 ||
-                   String.Compare(c.MrId, "Breaker_Pump1") == 0 ||
-                   String.Compare(c.MrId, "Discrete_Disc1") == 0 ||
-                   String.Compare(c.MrId, "Discrete_Disc2") == 0)
-                    lista.Add((DigitalPointCacheItem)c);
+                try
+                {
+                    tank1 = (AnalogPointCacheItem)model.Values.Where(x => x.MrId == "FluidLevel_Tank1").ToList()[0];
+                }
+                catch (Exception) { }
+
+                if (tank1 != null)
+                {
+                    if (tank1.RawValue > 90 && substationOne.TrueForAll(x => x.State == DState.ON))
+                    {
+                        Thread t = new Thread(EmptyTank1);
+                        t.Start();
+                    }
+                    else
+                        tank1 = null;
+                }
             }
 
-            if (lista.TrueForAll(x => x.State == DState.ON))
+
+            if (tank2 == null)
+            {
+                try
+                {
+                    tank2 = (AnalogPointCacheItem)model.Values.Where(x => x.MrId == "FluidLevel_Tank2").ToList()[0];
+                }
+                catch (Exception) { }
+
+                if (tank2 != null)
+                {
+                    if (tank2.RawValue > 90 && substationTwo.TrueForAll(x => x.State == DState.ON))
+                    {
+                        Thread t = new Thread(EmptyTank2);
+                        t.Start();
+                    }
+                    else
+                        tank2 = null;
+                }
+            }
+        }
+
+        private void FillLists()
+        {
+            if (substationOne.Count == 0)
+            {
+                foreach (var c in model.Values.Where(x => x.Type == PointType.BINARY_OUTPUT).ToList())
+                {
+                    if (String.Compare(c.MrId, "Breaker_1SwitchStatus") == 0 ||
+                       String.Compare(c.MrId, "Breaker_Pump1") == 0 ||
+                       String.Compare(c.MrId, "Discrete_Disc1") == 0 ||
+                       String.Compare(c.MrId, "Discrete_Disc2") == 0)
+                    {
+                        if(!substationOne.Contains((DigitalPointCacheItem)c))
+                            substationOne.Add((DigitalPointCacheItem)c);
+                    }
+                }
+            }
+
+            if (substationTwo.Count == 0)
+            {
+                foreach (var c in model.Values.Where(x => x.Type == PointType.BINARY_OUTPUT).ToList())
+                {
+                    if (String.Compare(c.MrId, "Breaker_2SwitchStatus") == 0 ||
+                       String.Compare(c.MrId, "Breaker_Pump2") == 0 ||
+                       String.Compare(c.MrId, "Breaker_Pump3") == 0 ||
+                       String.Compare(c.MrId, "Discrete_Disc3") == 0 ||
+                       String.Compare(c.MrId, "Discrete_Disc4") == 0)
+                    {
+                        if (!substationTwo.Contains((DigitalPointCacheItem)c))
+                            substationTwo.Add((DigitalPointCacheItem)c);
+                    }
+                }
+            }
+        }
+
+        private void SetCurrentAndVoltage()
+        {
+            if (substationOne.TrueForAll(x => x.State == DState.ON))
             {
                 foreach (var v in model.Values)
                 {
                     v.RawValue = 5;
                     v.NormalValue = 5;
                     if (String.Compare(v.MrId, "PT1Current_W1") == 0)
-                        SendCurrentVoltageMessage(v);
+                        SendMessage(v);
                     if (String.Compare(v.MrId, "PT1Current_W2") == 0)
-                        SendCurrentVoltageMessage(v);
+                        SendMessage(v);
 
                     v.RawValue = 220;
                     v.NormalValue = 220;
                     if (String.Compare(v.MrId, "PT1Voltage_W1") == 0)
-                        SendCurrentVoltageMessage(v);
+                        SendMessage(v);
                     if (String.Compare(v.MrId, "PT1Voltage_W2") == 0)
                     {
-                        SendCurrentVoltageMessage(v);
+                        SendMessage(v);
                         break;
                     }
                 }
@@ -100,38 +172,26 @@ namespace FrontEndProcessorService.Simulator
             }*/
         }
 
-        public void SetCurrentAndVoltageSubTwo()
+        private void SetCurrentAndVoltageSubTwo()
         {
-            List<DigitalPointCacheItem> lista = new List<DigitalPointCacheItem>();
-
-            foreach (var c in model.Values.Where(x => x.Type == PointType.BINARY_OUTPUT).ToList())
-            {
-                if (String.Compare(c.MrId, "Breaker_2SwitchStatus") == 0 ||
-                   String.Compare(c.MrId, "Breaker_Pump2") == 0 ||
-                   String.Compare(c.MrId, "Breaker_Pump3") == 0 ||
-                   String.Compare(c.MrId, "Discrete_Disc3") == 0 ||
-                   String.Compare(c.MrId, "Discrete_Disc4") == 0)
-                    lista.Add((DigitalPointCacheItem)c);
-            }
-
-            if (lista.TrueForAll(x => x.State == DState.ON))
+            if (substationTwo.TrueForAll(x => x.State == DState.ON))
             {
                 foreach (var v in model.Values)
                 {
                     v.RawValue = 5;
                     v.NormalValue = 5;
                     if (String.Compare(v.MrId, "PT2Current_W1") == 0)
-                        SendCurrentVoltageMessage(v);
+                        SendMessage(v);
                     if (String.Compare(v.MrId, "PT2Current_W2") == 0)
-                        SendCurrentVoltageMessage(v);
+                        SendMessage(v);
 
                     v.RawValue = 220;
                     v.NormalValue = 220;
                     if (String.Compare(v.MrId, "PT2Voltage_W1") == 0)
-                        SendCurrentVoltageMessage(v);
+                        SendMessage(v);
                     if (String.Compare(v.MrId, "PT2Voltage_W2") == 0)
                     {
-                        SendCurrentVoltageMessage(v);
+                        SendMessage(v);
                         break;
                     }
                 }
@@ -159,8 +219,44 @@ namespace FrontEndProcessorService.Simulator
                 }
             }*/
         }
+        
+        private void EmptyTank1()
+        {
+            if(tank1 != null)
+            {
+                while(tank1.RawValue >= tank1.NormalValue)
+                {
+                    tank1.RawValue -= 5;
 
-        public void SendCurrentVoltageMessage(BasePointCacheItem item)
+                    SendMessage(tank1);
+
+                    model.Values.Where(x => x.Gid == tank1.Gid).First().RawValue = tank1.RawValue;
+
+                    Thread.Sleep(5000);
+                }
+            }
+            tank1 = null;
+        }
+
+        private void EmptyTank2()
+        {
+            if (tank2 != null)
+            {
+                while (tank2.RawValue >= tank2.NormalValue)
+                {
+                    tank2.RawValue -= 5;
+
+                    SendMessage(tank2);
+
+                    model.Values.Where(x => x.Gid == tank2.Gid).First().RawValue = tank2.RawValue;
+
+                    Thread.Sleep(5000);
+                }
+                tank2 = null;
+            }
+        }
+
+        private void SendMessage(BasePointCacheItem item)
         {
             DNP3ApplicationObjectParameters p = new DNP3ApplicationObjectParameters(0xc1, (byte)DNP3FunctionCode.DIRECT_OPERATE, (ushort)TypeField.ANALOG_OUTPUT_16BIT, 0x28, 0x0001, item.Address, (uint)item.RawValue, 0x6405, 0x05, 0xc4, 0x0001, 0x0002, 0xc1);
             IDNP3Functions fn = DNP3FunctionFactory.CreateDNP3Function(p);
