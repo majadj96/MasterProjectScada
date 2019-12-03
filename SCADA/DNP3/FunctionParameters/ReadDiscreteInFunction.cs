9b0ae9145df5b76c55a5a5d822f0f5fbb5b0931c
Nemanja Vasiljevic <nemanjav1996@hotmail.com>
2019-12-03 20:54:34 +0100

    ReadDiscreteInput function is done

---
+++ SCADA/DNP3/FunctionParameters/ReadDiscreteInFunction.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DNP3.DNP3Functions;
+using ScadaCommon;
+using ScadaCommon.CRCCalculator;
+
+namespace DNP3.FunctionParameters
+{
+    public class ReadDiscreteInFunction : DNP3Functions
+    {
+        public ReadDiscreteInFunction(DNP3ApplicationObjectParameters commandParameters) : base(commandParameters)
+        {
+        }
+
+        public override byte[] PackRequest()
+        {
+            byte[] dnp3Request = new byte[20];
+
+            Buffer.BlockCopy(BitConverter.GetBytes((short)CommandParameters.Start), 0, dnp3Request, 0, 2);
+            dnp3Request[2] = 0x0d;
+            dnp3Request[3] = CommandParameters.Control;
+            Buffer.BlockCopy(BitConverter.GetBytes((short)CommandParameters.Destination), 0, dnp3Request, 4, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes((short)CommandParameters.Source), 0, dnp3Request, 6, 2);
+
+            ushort crc = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                CRCCalculator.computeCRC(dnp3Request[i], ref crc);
+            }
+            crc = (ushort)(~crc);
+
+            Buffer.BlockCopy(BitConverter.GetBytes((short)crc), 0, dnp3Request, 8, 2);
+            dnp3Request[10] = CommandParameters.TransportHeader;
+            dnp3Request[11] = CommandParameters.AplicationControl;
+            dnp3Request[12] = CommandParameters.FunctionCode;
+            dnp3Request[13] = BitConverter.GetBytes((short)CommandParameters.TypeField)[1];
+            dnp3Request[14] = BitConverter.GetBytes((short)CommandParameters.TypeField)[0];
+            dnp3Request[15] = CommandParameters.Qualifier;
+            dnp3Request[16] = (byte)CommandParameters.Range;
+            dnp3Request[17] = (byte)CommandParameters.Range;
+
+            ushort crc2 = 0;
+            for (int i = 10; i < 18; i++)
+            {
+                CRCCalculator.computeCRC(dnp3Request[i], ref crc2);
+            }
+            crc2 = (ushort)(~crc2);
+            Buffer.BlockCopy(BitConverter.GetBytes(crc2), 0, dnp3Request, 18, 2);
+
+            return dnp3Request;
+        }
+
+        public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] receivedBytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
