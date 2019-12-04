bdbcfc40ffebe424b8e589197ced4322be6912eb
Nemanja Vasiljevic <nemanjav1996@hotmail.com>
2019-12-04 14:43:10 +0100

    FunctionExecutor recieveBytes length calculated

--- SCADA/ScadaCommon/Connection/FunctionExecutor.cs
+++ SCADA/ScadaCommon/Connection/FunctionExecutor.cs
@@ -116,17 +116,27 @@ namespace ScadaCommon.Connection
 							byte[] message;
 							byte[] header = this.connection.RecvBytes(10);
 							byte payLoadSize = 0;
+                            int len = 0;
 							unchecked
 							{
 								payLoadSize = (byte)BitConverter.ToChar(header, 2);
 							}
                             byte[] payload;
-                            if (payLoadSize == 28)
-                                payload = this.connection.RecvBytes((int)payLoadSize - 1); //writeDigitalOutput
-                            else if (payLoadSize == 20)
-                                payload = this.connection.RecvBytes((int)payLoadSize - 3); // writeAnalogOutput
+
+                            //Duzina poruke posle header-a (heder je duzine 5) racuna se tako sto od ukupne duzine oduzmemo header
+                            // i na tu duzinu dodajemo duzinu svih crc-ova koji su na svakih 16 bajtova
+                            payLoadSize = (byte)(payLoadSize - 5);
+
+                            if (payLoadSize % 16 == 0)
+                            {
+                                len = payLoadSize + (payLoadSize / 16) * 2;
+                            }
                             else
-                                payload = null;
+                            {
+                                len = (payLoadSize / 16) == 0 ? (byte)(payLoadSize + 2) : (byte)(payLoadSize + (payLoadSize / 16) * 2 + 2);
+                            }
+
+                            payload = this.connection.RecvBytes(len);
 
                             message = new byte[header.Length + payload.Length];
 							Buffer.BlockCopy(header, 0, message, 0, 10);
