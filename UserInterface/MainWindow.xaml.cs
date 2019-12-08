e7a0e65ea1009a2893409091b8da0734070090d8
Maja <maya.simsa1@gmail.com>
2019-12-08 10:32:48 +0100

    Implementing schema

--- UserInterface/MainWindow.xaml.cs
+++ UserInterface/MainWindow.xaml.cs
@@ -18,11 +18,62 @@ namespace UserInterface
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            DataContext = new MainViewModel(this);
             InitializeComponent();
         }
+
+
+        //private void breaker_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    if (breaker.X1 == 0)
+        //    {
+        //        breaker.X1 = 20;
+        //        statistics.Text = "Breaker status: OFF";
+        //        breaker.Stroke = line_2.Stroke = line_3.Stroke = getColor(Colors.Black);
+        //    }
+        //    else if (breaker.X1 == 20)
+        //    {
+        //        breaker.X1 = 0;
+        //        if (disconector.X1 == 0)
+        //        {
+        //            breaker.Stroke = line_2.Stroke = line_3.Stroke = getColor(Colors.Yellow);
+        //        }
+        //        else if (disconector.X1 == 20)
+        //        {
+        //            breaker.Stroke = line_2.Stroke = line_3.Stroke = getColor(Colors.Black);
+        //        }
+        //        statistics.Text = "Breaker status: ON";
+        //    }
+
+        //}
+
+        //private void disconector_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    if (disconector.X1 == 0)
+        //    {
+        //        disconector.X1 = 20;
+        //        disconector.Stroke =
+        //        disconector.Stroke = line_1.Stroke = breaker.Stroke = line_2.Stroke = line_3.Stroke = getColor(Colors.Black);
+        //        statistics.Text = "Disconector status: OFF";
+        //    }
+        //    else if (disconector.X1 == 20)
+        //    {
+        //        disconector.X1 = 0;
+        //        disconector.Stroke = line_1.Stroke  = getColor(Colors.Yellow);
+        //        if (breaker.X1 == 0)
+        //        {
+        //            breaker.Stroke = line_2.Stroke = line_3.Stroke = getColor(Colors.Yellow);
+        //        }
+        //        statistics.Text = "Disconector status: ON";
+
+        //    }
+        //}
+
+
     }
 }
---
+++ UserInterface/MainWindowViewModel.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace UserInterface
+{
+    public class MyLine
+    {
+        public int X1 { get; set; }
+        public int Y1 { get; set; }
+        public int X2 { get; set; }
+        public int Y2 { get; set; }
+    }
+
+    class MainViewModel : INotifyPropertyChanged
+    {
+
+        public MainViewModel(Window window)
+        {
+            window.WindowState = WindowState.Maximized;
+            window.WindowStyle = WindowStyle.None;
+            Line_1 = new MyLine();
+            Line_1.X1 = Line_1.X2 = Line_1.Y1 = 0;
+            Line_1.Y2 = 40;
+            //setMesh();
+        }
+        public Window Window { get; set; }
+        public MyLine line_1 { get; set; }
+        public MyLine disconector { get; set; }
+        public MyLine breaker { get; set; }
+        public MyLine line_2 { get; set; }
+        public MyLine line_3 { get; set; }
+
+
+        public MyLine Line_1
+        {
+            get
+            {
+                return line_1 ;
+            }
+            set
+            {
+                line_1 = value;
+                OnPropertyChanged("Line_1");
+            }
+        }
+        public MyLine Breker
+        {
+            get
+            {
+                return breaker;
+            }
+            set
+            {
+                breaker = value;
+                OnPropertyChanged("Breker");
+            }
+        }
+        public MyLine Disconector
+        {
+            get
+            {
+                return disconector;
+            }
+            set
+            {
+                disconector = value;
+                OnPropertyChanged("Disconector");
+            }
+        }
+        public MyLine Line_2
+        {
+            get
+            {
+                return line_2;
+            }
+            set
+            {
+                line_2 = value;
+                OnPropertyChanged("Line_2");
+            }
+        }
+        public MyLine Line_3
+        {
+            get
+            {
+                return line_3;
+            }
+            set
+            {
+                line_3 = value;
+                OnPropertyChanged("Line_3");
+            }
+        }
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        private SolidColorBrush getColor(Color color)
+        {
+            return new SolidColorBrush(color);
+        }
+
+        private void setMesh()
+        {
+            if (disconector.X1 == 0)
+            {
+               // line_1.Stroke = getColor(Colors.Yellow);
+            }
+        }
+        public void Prijava(string loz)
+        {
+
+        }
+
+        public void Registracija()
+        {
+
+        }
+
+    }
+}
