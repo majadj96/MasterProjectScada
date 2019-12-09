0829fd08ca8a052aa2ebb17754363af346ff80c3
barefoot155 <bosabratic@yahoo.com>
2019-12-09 17:56:06 +0100

    Wires classes added

---
+++ DataModel/Wires/PowerTransformer.cs
@@ -0,0 +1,16 @@
+﻿using DataModel.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel.Wires
+{
+    public class PowerTransformer : Equipment
+    {
+        public PowerTransformer(string gID) : base(gID)
+        {
+        }
+    }
+}
---
+++ DataModel/Wires/TapChanger.cs
@@ -0,0 +1,24 @@
+﻿using DataModel.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel.Wires
+{
+    public class TapChanger:PowerSystemResource
+    {
+        private int highStep;
+        private int lowStep;
+        private int normalStep;
+
+        public TapChanger(string gID) : base(gID)
+        {
+        }
+
+        public int HighStep { get => highStep; set => highStep = value; }
+        public int LowStep { get => lowStep; set => lowStep = value; }
+        public int NormalStep { get => normalStep; set => normalStep = value; }
+    }
+}
