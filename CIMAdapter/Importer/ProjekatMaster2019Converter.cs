ace4140dab9015ab23872849ced0ab4137206fef
barefoot155 <bosabratic@yahoo.com>
2019-12-09 20:35:16 +0100

    CIMParser and CIMAdapter added

---
+++ CIMAdapter/Importer/ProjekatMaster2019Converter.cs
@@ -0,0 +1,25 @@
+﻿namespace FTN.ESI.SIMES.CIM.CIMAdapter.Importer
+{
+	using Common;
+    using Common.GDA;
+
+    /// <summary>
+    /// PowerTransformerConverter has methods for populating
+    /// ResourceDescription objects using ProjekatMaster2019CIMProfile_Labs objects.
+    /// </summary>
+    public static class ProjekatMaster2019Converter
+	{
+
+        #region Populate ResourceDescription
+
+        //TODO
+
+        #endregion Populate ResourceDescription
+
+        #region Enums convert
+
+        //TODO
+
+		#endregion Enums convert
+	}
+}
---
+++ CIMAdapter/Importer/ProjekatMaster2019Importer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using CIM.Model;
+using Common;
+using Common.GDA;
+using FTN.ESI.SIMES.CIM.CIMAdapter.Manager;
+
+namespace FTN.ESI.SIMES.CIM.CIMAdapter.Importer
+{
+    /// <summary>
+    /// ProjekatMaster2019Importer
+    /// </summary>
+    public class ProjekatMaster2019Importer
+    {
+        ///// <summary> Singleton </summary>
+        private static ProjekatMaster2019Importer ptImporter = null;
+        private static object singletoneLock = new object();
+
+        private ConcreteModel concreteModel;
+        private Delta delta;
+        private ImportHelper importHelper;
+        private TransformAndLoadReport report;
+
+
+        #region Properties
+        public static ProjekatMaster2019Importer Instance
+        {
+            get
+            {
+                if (ptImporter == null)
+                {
+                    lock (singletoneLock)
+                    {
+                        if (ptImporter == null)
+                        {
+                            ptImporter = new ProjekatMaster2019Importer();
+                            ptImporter.Reset();
+                        }
+                    }
+                }
+                return ptImporter;
+            }
+        }
+
+        public Delta NMSDelta
+        {
+            get
+            {
+                return delta;
+            }
+        }
+        #endregion Properties
+
+
+        public void Reset()
+        {
+            concreteModel = null;
+            delta = new Delta();
+            importHelper = new ImportHelper();
+            report = null;
+        }
+
+        public TransformAndLoadReport CreateNMSDelta(ConcreteModel cimConcreteModel)
+        {
+            LogManager.Log("Importing ProjekatMaster2019 Elements...", LogLevel.Info);
+            report = new TransformAndLoadReport();
+            concreteModel = cimConcreteModel;
+            delta.ClearDeltaOperations();
+
+            if ((concreteModel != null) && (concreteModel.ModelMap != null))
+            {
+                try
+                {
+                    // convert into DMS elements
+                    ConvertModelAndPopulateDelta();
+                }
+                catch (Exception ex)
+                {
+                    string message = string.Format("{0} - ERROR in data import - {1}", DateTime.Now, ex.Message);
+                    LogManager.Log(message);
+                    report.Report.AppendLine(ex.Message);
+                    report.Success = false;
+                }
+            }
+            LogManager.Log("Importing PowerTransformer Elements - END.", LogLevel.Info);
+            return report;
+        }
+
+        /// <summary>
+        /// Method performs conversion of network elements from CIM based concrete model into DMS model.
+        /// </summary>
+        private void ConvertModelAndPopulateDelta()
+        {
+            LogManager.Log("Loading elements and creating delta...", LogLevel.Info);
+
+            // import all concrete model types (DMSType enum)
+            
+            //TODO
+
+            LogManager.Log("Loading elements and creating delta completed.", LogLevel.Info);
+        }
+
+        #region Import
+
+        //TODO
+        
+        #endregion Import
+    }
+}
+
