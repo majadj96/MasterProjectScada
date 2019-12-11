e2650f4ddeff690a840e1fd330f162273ae6f0b0
barefoot155 <bosabratic@yahoo.com>
2019-12-11 17:52:36 +0100

    Importer methods implemented.

--- CIMAdapter/Importer/ProjekatMaster2019Importer.cs
+++ CIMAdapter/Importer/ProjekatMaster2019Importer.cs
@@ -102,8 +102,240 @@ namespace FTN.ESI.SIMES.CIM.CIMAdapter.Importer
 
         #region Import
 
-        //TODO
-        
+        private void ImportAsynchronousMachine()
+        {
+            SortedDictionary<string, object> cimAsyncMachines = concreteModel.GetAllObjectsOfType("FTN.AsynchronousMachine");
+            if (cimAsyncMachines != null)
+            {
+                foreach (KeyValuePair<string, object> cimAsyncMachinePair in cimAsyncMachines)
+                {
+                    FTN.AsynchronousMachine cimAsynchronousMachine = cimAsyncMachinePair.Value as FTN.AsynchronousMachine;
+
+                    ResourceDescription rd = CreateAsynchronousMachineResourceDescription(cimAsynchronousMachine);
+                    if (rd != null)
+                    {
+                        delta.AddDeltaOperation(DeltaOpType.Insert, rd, true);
+                        report.Report.Append("AsynchronousMachine ID = ").Append(cimAsynchronousMachine.ID).Append(" SUCCESSFULLY converted to GID = ").AppendLine(rd.Id.ToString());
+                    }
+                    else
+                    {
+                        report.Report.Append("AsynchronousMachine ID = ").Append(cimAsynchronousMachine.ID).AppendLine(" FAILED to be converted");
+                    }
+                }
+                report.Report.AppendLine();
+            }
+        }
+
+        private ResourceDescription CreateAsynchronousMachineResourceDescription(FTN.AsynchronousMachine cimAsyncMachine)
+        {
+            ResourceDescription rd = null;
+            if (cimAsyncMachine != null)
+            {
+                long gid = ModelCodeHelper.CreateGlobalId(0, (short)DMSType.ASYNCHRONOUSMACHINE, importHelper.CheckOutIndexForDMSType(DMSType.ASYNCHRONOUSMACHINE));
+                rd = new ResourceDescription(gid);
+                importHelper.DefineIDMapping(cimAsyncMachine.ID, gid);
+
+                ////populate ResourceDescription
+                ProjekatMaster2019Converter.PopulateAsynchronousMachineProperties(cimAsyncMachine, rd, importHelper, report);
+            }
+            return rd;
+        }
+
+        private void ImportAnalog()
+        {
+            SortedDictionary<string, object> cimAnalogs = concreteModel.GetAllObjectsOfType("FTN.Analog");
+            if (cimAnalogs != null)
+            {
+                foreach (KeyValuePair<string, object> cimAnalogPair in cimAnalogs)
+                {
+                    FTN.Analog cimAnalog = cimAnalogPair.Value as FTN.Analog;
+
+                    ResourceDescription rd = CreateAnalogResourceDescription(cimAnalog);
+                    if (rd != null)
+                    {
+                        delta.AddDeltaOperation(DeltaOpType.Insert, rd, true);
+                        report.Report.Append("Analog ID = ").Append(cimAnalog.ID).Append(" SUCCESSFULLY converted to GID = ").AppendLine(rd.Id.ToString());
+                    }
+                    else
+                    {
+                        report.Report.Append("Analog ID = ").Append(cimAnalog.ID).AppendLine(" FAILED to be converted");
+                    }
+                }
+                report.Report.AppendLine();
+            }
+        }
+
+        private ResourceDescription CreateAnalogResourceDescription(FTN.Analog cimAnalog)
+        {
+            ResourceDescription rd = null;
+            if (cimAnalog != null)
+            {
+                long gid = ModelCodeHelper.CreateGlobalId(0, (short)DMSType.ANALOG, importHelper.CheckOutIndexForDMSType(DMSType.ANALOG));
+                rd = new ResourceDescription(gid);
+                importHelper.DefineIDMapping(cimAnalog.ID, gid);
+
+                ////populate ResourceDescription
+                ProjekatMaster2019Converter.PopulateAnalogProperties(cimAnalog, rd, importHelper, report);
+            }
+            return rd;
+        }
+
+        private void ImportACLineSegment()
+        {
+            SortedDictionary<string, object> cimACLineSegments = concreteModel.GetAllObjectsOfType("FTN.ACLineSegment");
+            if (cimACLineSegments != null)
+            {
+                foreach (KeyValuePair<string, object> cimACLineSegmentPair in cimACLineSegments)
+                {
+                    FTN.ACLineSegment cimACLineSegment = cimACLineSegmentPair.Value as FTN.ACLineSegment;
+
+                    ResourceDescription rd = CreateACLineSegmentResourceDescription(cimACLineSegment);
+                    if (rd != null)
+                    {
+                        delta.AddDeltaOperation(DeltaOpType.Insert, rd, true);
+                        report.Report.Append("ACLineSegment ID = ").Append(cimACLineSegment.ID).Append(" SUCCESSFULLY converted to GID = ").AppendLine(rd.Id.ToString());
+                    }
+                    else
+                    {
+                        report.Report.Append("ACLineSegment ID = ").Append(cimACLineSegment.ID).AppendLine(" FAILED to be converted");
+                    }
+                }
+                report.Report.AppendLine();
+            }
+        }
+
+        private ResourceDescription CreateACLineSegmentResourceDescription(FTN.ACLineSegment cimACLineSegment)
+        {
+            ResourceDescription rd = null;
+            if (cimACLineSegment != null)
+            {
+                long gid = ModelCodeHelper.CreateGlobalId(0, (short)DMSType.ACLINESEGMENT, importHelper.CheckOutIndexForDMSType(DMSType.ACLINESEGMENT));
+                rd = new ResourceDescription(gid);
+                importHelper.DefineIDMapping(cimACLineSegment.ID, gid);
+
+                ////populate ResourceDescription
+                ProjekatMaster2019Converter.PopulateACLineSegmentProperties(cimACLineSegment, rd, importHelper, report);
+            }
+            return rd;
+        }
+
+        private void ImportBreaker()
+        {
+            SortedDictionary<string, object> cimBreakers = concreteModel.GetAllObjectsOfType("FTN.Breaker");
+            if (cimBreakers != null)
+            {
+                foreach (KeyValuePair<string, object> cimBreakerPair in cimBreakers)
+                {
+                    FTN.Breaker cimBreaker = cimBreakerPair.Value as FTN.Breaker;
+
+                    ResourceDescription rd = CreateBreakerResourceDescription(cimBreaker);
+                    if (rd != null)
+                    {
+                        delta.AddDeltaOperation(DeltaOpType.Insert, rd, true);
+                        report.Report.Append("Breaker ID = ").Append(cimBreaker.ID).Append(" SUCCESSFULLY converted to GID = ").AppendLine(rd.Id.ToString());
+                    }
+                    else
+                    {
+                        report.Report.Append("Breaker ID = ").Append(cimBreaker.ID).AppendLine(" FAILED to be converted");
+                    }
+                }
+                report.Report.AppendLine();
+            }
+        }
+
+        private ResourceDescription CreateBreakerResourceDescription(FTN.Breaker cimBreaker)
+        {
+            ResourceDescription rd = null;
+            if (cimBreaker != null)
+            {
+                long gid = ModelCodeHelper.CreateGlobalId(0, (short)DMSType.BREAKER, importHelper.CheckOutIndexForDMSType(DMSType.BREAKER));
+                rd = new ResourceDescription(gid);
+                importHelper.DefineIDMapping(cimBreaker.ID, gid);
+
+                ////populate ResourceDescription
+                ProjekatMaster2019Converter.PopulateBreakerProperties(cimBreaker, rd, importHelper, report);
+            }
+            return rd;
+        }
+
+        private void ImportConnectivityNode()
+        {
+            SortedDictionary<string, object> cimConnectivityNodes = concreteModel.GetAllObjectsOfType("FTN.ConnectivityNode");
+            if (cimConnectivityNodes != null)
+            {
+                foreach (KeyValuePair<string, object> cimConnectivityNodePair in cimConnectivityNodes)
+                {
+                    FTN.ConnectivityNode cimConnectivityNode = cimConnectivityNodePair.Value as FTN.ConnectivityNode;
+
+                    ResourceDescription rd = CreateConnectivityNodeResourceDescription(cimConnectivityNode);
+                    if (rd != null)
+                    {
+                        delta.AddDeltaOperation(DeltaOpType.Insert, rd, true);
+                        report.Report.Append("ConnectivityNode ID = ").Append(cimConnectivityNode.ID).Append(" SUCCESSFULLY converted to GID = ").AppendLine(rd.Id.ToString());
+                    }
+                    else
+                    {
+                        report.Report.Append("ConnectivityNode ID = ").Append(cimConnectivityNode.ID).AppendLine(" FAILED to be converted");
+                    }
+                }
+                report.Report.AppendLine();
+            }
+        }
+
+        private ResourceDescription CreateConnectivityNodeResourceDescription(FTN.ConnectivityNode cimConnectivityNode)
+        {
+            ResourceDescription rd = null;
+            if (cimConnectivityNode != null)
+            {
+                long gid = ModelCodeHelper.CreateGlobalId(0, (short)DMSType.CONNECTIVITYNODE, importHelper.CheckOutIndexForDMSType(DMSType.CONNECTIVITYNODE));
+                rd = new ResourceDescription(gid);
+                importHelper.DefineIDMapping(cimConnectivityNode.ID, gid);
+
+                ////populate ResourceDescription
+                ProjekatMaster2019Converter.PopulateConnectivityNodeProperties(cimConnectivityNode, rd, importHelper, report);
+            }
+            return rd;
+        }
+
+        private void ImportDisconnector()
+        {
+            SortedDictionary<string, object> cimDisconnectors = concreteModel.GetAllObjectsOfType("FTN.Disconnector");
+            if (cimDisconnectors != null)
+            {
+                foreach (KeyValuePair<string, object> cimDisconnectorPair in cimDisconnectors)
+                {
+                    FTN.Disconnector cimDisconnector = cimDisconnectorPair.Value as FTN.Disconnector;
+
+                    ResourceDescription rd = CreateDisconnectorResourceDescription(cimDisconnector);
+                    if (rd != null)
+                    {
+                        delta.AddDeltaOperation(DeltaOpType.Insert, rd, true);
+                        report.Report.Append("Disconnector ID = ").Append(cimDisconnector.ID).Append(" SUCCESSFULLY converted to GID = ").AppendLine(rd.Id.ToString());
+                    }
+                    else
+                    {
+                        report.Report.Append("Disconnector ID = ").Append(cimDisconnector.ID).AppendLine(" FAILED to be converted");
+                    }
+                }
+                report.Report.AppendLine();
+            }
+        }
+
+        private ResourceDescription CreateDisconnectorResourceDescription(FTN.Disconnector cimDisconnector)
+        {
+            ResourceDescription rd = null;
+            if (cimDisconnector != null)
+            {
+                long gid = ModelCodeHelper.CreateGlobalId(0, (short)DMSType.DISCONNECTOR, importHelper.CheckOutIndexForDMSType(DMSType.DISCONNECTOR));
+                rd = new ResourceDescription(gid);
+                importHelper.DefineIDMapping(cimDisconnector.ID, gid);
+
+                ////populate ResourceDescription
+                ProjekatMaster2019Converter.PopulateDisconnectorProperties(cimDisconnector, rd, importHelper, report);
+            }
+            return rd;
+        }
+
         #endregion Import
     }
 }
