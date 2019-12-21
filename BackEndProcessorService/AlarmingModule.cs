87fe98aaf10adee70b68596e3431aeb894f31a2d
Aleksandar Dondur <aleksandar.dondur@gmail.com>
2019-12-21 17:41:14 +0100

    Starting developing backend process

---
+++ BackEndProcessorService/AlarmingModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScadaCommon.BackEnd_FrontEnd;
+
+namespace BackEndProcessorService
+{
+    public class AlarmingModule : IProcessingData
+    {
+        public void Process(IProcessingObject processingObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
