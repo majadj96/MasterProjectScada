bc80f31d661cef158b97c6850bf5ac242b9576d7
tamir-cohen <obrenm4@yahoo.com>
2019-12-21 17:54:13 +0100

    PSR IReference methods updated

--- DataModel/Core/PowerSystemResource.cs
+++ DataModel/Core/PowerSystemResource.cs
@@ -85,12 +85,39 @@ namespace DataModel.Core
 
         public override void AddReference(ModelCode referenceId, long globalId)
         {
-            base.AddReference(referenceId, globalId);
+            switch (referenceId)
+            {
+                case ModelCode.MEASUREMENT_PSR:
+                    measurements.Add(globalId);
+                    break;
+
+                default:
+                    base.AddReference(referenceId, globalId);
+                    break;
+            }
         }
 
         public override void RemoveReference(ModelCode referenceId, long globalId)
         {
-            base.RemoveReference(referenceId, globalId);
+            switch (referenceId)
+            {
+                case ModelCode.MEASUREMENT_PSR:
+
+                    if (measurements.Contains(globalId))
+                    {
+                        measurements.Remove(globalId);
+                    }
+                    else
+                    {
+                        CommonTrace.WriteTrace(CommonTrace.TraceWarning, "Entity (GID = 0x{0:x16}) doesn't contain reference 0x{1:x16}.", this.GID, globalId);
+                    }
+
+                    break;
+
+                default:
+                    base.RemoveReference(referenceId, globalId);
+                    break;
+            }
         }
 
         #endregion
