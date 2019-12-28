0605200b9a0bd3114128b166a862142705ee85d5
KristinaPolih <christina.polih@yahoo.com>
2019-12-28 10:38:16 +0100

    Refactoring

--- SCADA/ProcessingModule/Acquisitor.cs
+++ SCADA/ProcessingModule/Acquisitor.cs
@@ -1,10 +1,6 @@
-﻿using DNP3.DNP3Functions;
-using ScadaCommon.Interfaces;
-using ScadaCommon;
+﻿using ScadaCommon.Interfaces;
 using System;
 using System.Threading;
-using DNP3;
-using System.Collections.Generic;
 
 namespace ProcessingModule
 {
@@ -12,11 +8,11 @@ namespace ProcessingModule
     /// Class containing logic for periodic polling.
     /// </summary>
     public class Acquisitor : IDisposable
-	{
-		private AutoResetEvent acquisitionTrigger;
+    {
+        private AutoResetEvent acquisitionTrigger;
         private IProcessingManager processingManager;
         private Thread acquisitionWorker;
-		private IConfiguration configuration;
+        private IConfiguration configuration;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Acquisitor"/> class.
@@ -26,38 +22,38 @@ namespace ProcessingModule
         /// <param name="stateUpdater">The state updater.</param>
         /// <param name="configuration">The configuration.</param>
 		public Acquisitor(AutoResetEvent acquisitionTrigger, IProcessingManager processingManager, IConfiguration configuration)
-		{
-			this.acquisitionTrigger = acquisitionTrigger;
-			this.processingManager = processingManager;
-			this.configuration = configuration;
-			this.InitializeAcquisitionThread();
-			this.StartAcquisitionThread();
-		}
+        {
+            this.acquisitionTrigger = acquisitionTrigger;
+            this.processingManager = processingManager;
+            this.configuration = configuration;
+            this.InitializeAcquisitionThread();
+            this.StartAcquisitionThread();
+        }
 
-		#region Private Methods
+        #region Private Methods
 
         /// <summary>
         /// Initializes the acquisition thread.
         /// </summary>
-		private void InitializeAcquisitionThread()
-		{
-			this.acquisitionWorker = new Thread(Acquisition_DoWork);
-			this.acquisitionWorker.Name = "Acquisition thread";
-		}
+        private void InitializeAcquisitionThread()
+        {
+            this.acquisitionWorker = new Thread(Acquisition_DoWork);
+            this.acquisitionWorker.Name = "Acquisition thread";
+        }
 
         /// <summary>
         /// Starts the acquisition thread.
         /// </summary>
 		private void StartAcquisitionThread()
-		{
-			acquisitionWorker.Start();
-		}
+        {
+            acquisitionWorker.Start();
+        }
 
         /// <summary>
         /// Acquisitor thread logic.
         /// </summary>
 		private void Acquisition_DoWork()
-		{
+        {
             //Class 0
             try
             {
@@ -66,22 +62,17 @@ namespace ProcessingModule
 
                 while (true)
                 {
-                    //if (FunctionExecutor.acquisitionDone)
+                    state = acquisitionTrigger.WaitOne();
+
+                    if (cnt < configuration.Class0Acquisition && state)
+                        cnt++;
+
+                    if (cnt == configuration.Class0Acquisition)
                     {
-                        state = acquisitionTrigger.WaitOne();
+                        processingManager.ExecuteReadCommand(configuration.GetConfigurationItems()[configuration.GetConfigurationItems().Count - 1], 0x00, 0, 0x00, 0x00);
 
-                        if (cnt < configuration.Class0Acquisition && state)
-                            cnt++;
-
-                        if (cnt == configuration.Class0Acquisition)
-                        {
-                            processingManager.ExecuteReadCommand(configuration.GetConfigurationItems()[configuration.GetConfigurationItems().Count - 1], 0x00, 0, 0x00, 0x00);
-                            FunctionExecutor.acquisitionDone = false;
-                            cnt = 0;
-                        }
+                        cnt = 0;
                     }
-                    //else
-                        //acquisitionTrigger.WaitOne();
                 }
             }
             catch (Exception ex)
@@ -89,13 +80,13 @@ namespace ProcessingModule
                 string message = $"{ex.TargetSite.ReflectedType.Name}.{ex.TargetSite.Name}: {ex.Message}";
             }
         }
-        
+
         #endregion Private Methods
 
         /// <inheritdoc />
         public void Dispose()
-		{
-			acquisitionWorker.Abort();
+        {
+            acquisitionWorker.Abort();
         }
-	}
+    }
 }
