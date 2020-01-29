9f58ad03084db0051cba21dd928436b2f84045f2
Maja <maya.simsa1@gmail.com>
2020-01-29 16:44:36 +0100

    Removed NMS for testing

--- PubSub/Program.cs
+++ PubSub/Program.cs
@@ -1,5 +1,4 @@
-﻿using NMS;
-using PubSubCommon;
+﻿using PubSubCommon;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,18 +16,6 @@ namespace PubSub
         {
             ServerHost serverHost = new ServerHost();
             Console.WriteLine("Hosts are opened");
-
-            //PubSub ovde poziva CE i simulira poziv koji treba da dodje od strane NMS-a (TEST, OBRISATI)
-
-            //ModelUpdateProxy proxy = new ModelUpdateProxy("net.tcp://localhost:50000/CE");
-            //proxy.UpdateModel(new Common.GDA.Delta());
-            //NetTcpBinding netTcpbinding = new NetTcpBinding(SecurityMode.None);
-            //EndpointAddress endpointAddress = new EndpointAddress("net.tcp://localhost:10000/CE");
-            //InstanceContext context = new InstanceContext(callbackinstance);
-            //ChannelFactory<IModelUpdateContract> channelFactory = new ChannelFactory<IModelUpdateContract>(netTcpbinding, endpointAddress);
-            //var _proxy = channelFactory.CreateChannel();
-            //_proxy.UpdateModel(new Common.GDA.Delta());
-
             Console.ReadLine();
         }
     }
