18d9c9570583113a4c2952e50767623b9dea6ed9
tamir-cohen <obrenm4@yahoo.com>
2020-01-29 17:58:25 +0100

    Pubsub notifies NMS to update UI

--- PubSub/PubSubEngine/SubNMS.cs
+++ PubSub/PubSubEngine/SubNMS.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using TransactionManagerContracts;
 
 namespace PubSub.PubSubEngine
 {
@@ -19,6 +20,20 @@ namespace PubSub.PubSubEngine
             Console.WriteLine("Sab se lepo");
             IPubNMS subscriber = OperationContext.Current.GetCallbackChannel<IPubNMS>();
             Filter.AddSubscriber(topicName, subscriber);
+
+            //Notify NMS to send current model to UI
+            INotifyNMS proxy = CreateNMSProxy();
+            proxy.UpdateUIModel();
+        }
+
+        private INotifyNMS CreateNMSProxy()
+        {
+            NetTcpBinding netTcpbinding = new NetTcpBinding(SecurityMode.None);
+            EndpointAddress endpointAddress = new EndpointAddress("net.tcp://localhost:10010/NetworkModelService/NotifyNMS");
+            ChannelFactory<INotifyNMS> channelFactory = new ChannelFactory<INotifyNMS>(netTcpbinding, endpointAddress);
+            INotifyNMS proxy = channelFactory.CreateChannel();
+
+            return proxy;
         }
 
         public void UnSubscribe(string topicName)
