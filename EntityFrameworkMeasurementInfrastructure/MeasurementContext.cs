7c234e4a20ee21ee54d2cd55cb0c242b9ad461a8
PC <sretennenadovic@gmail.com>
2020-03-04 19:16:23 +0100

    Repository pattern started

---
+++ EntityFrameworkMeasurementInfrastructure/MeasurementContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkMeasurementInfrastructure
+{
+    public class MeasurementContext : DbContext
+    {
+        public MeasurementContext() : base("name=connectionString")
+        {
+
+        }
+
+        public DbSet<Measurement> Measurements { get; set; }
+    }
+}
---
+++ RepositoryCore/Interfaces/IMeasurementRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryCore.Interfaces
+{
+    public interface IMeasurementRepository
+    {
+        void Add(Measurement measurement);
+        void GetAllMeasurementsByGid(long gid);
+        void GetAllMeasurementsByTime(DateTime startTime, DateTime endTime, long gid);
+    }
+}
