b66c7b8efa7cc721da81603d4896578d7c66b1d8
dmilosevic@live.com <dmilosevic@live.com>
2020-06-26 14:28:24 +0200

    Delta Repository interface and implementation added

---
+++ NetworkModelService/DeltaDB/Interfaces/IDeltaRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkModelService.DeltaDB.Interfaces
+{
+    [ServiceContract]
+    public interface IDeltaRepository
+    {
+        [OperationContract]
+        void Add(DeltaDBModel delta);
+
+        [OperationContract]
+        List<DeltaDBModel> GetAllDeltas();
+    }
+}
