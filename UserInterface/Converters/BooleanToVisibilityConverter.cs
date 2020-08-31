b1112c7e99b5175abcf17914d99fc5f4f3c7caa3
tamir-cohen <obrenm4@yahoo.com>
2020-08-31 18:27:32 +0200

    Service fabric application added

--- UserInterface/Converters/BooleanToVisibilityConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace UserInterface.Converters
-{
-    public sealed class BooleanToVisibilityConverter : BooleanConverter<Visibility>
-    {
-        public BooleanToVisibilityConverter() : base(Visibility.Visible, Visibility.Collapsed) { }
-    }
-}
